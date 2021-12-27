411a6be69cfe0614fbe01a9f77f95e2a17c07077
Simpler one (Koba) <40133262+simpler-one@users.noreply.github.com>
2021-12-27 22:41:56 +0900

    Create index.vue

---
+++ src/components/modal/dialog/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <foo-modal
+    :class="class"
+    :visible="visible"
+    :persistent="mandatory"
+    @input="onInput"
+  >
+    <div class="dialog-head">
+      <foo-icon v-if="icon" :color="icon.color">{{icon.name}}</foo-icon>
+      {{title}}
+    </div>
+
+    <div class="dialog-body">
+      <p v-if="msg of messages">{{msg}}</p>
+    </div>
+
+    <div class="dialog-foot">
+      <span v-for="btn of buttons">
+        <foo-btn :class="btn.styleClass" @click="onButtonClicked(btn)">{{btn.text}}</foo-btn>
+      <span>
+    </div>
+  </foo-modal>
+</template>
+
+
+<script>
+const DEFAULT_ICON = { type: "", color: "" };
+
+
+export default {
+  props: {
+    class: { type: String, required: false, default: "" },
+    bgColor: { type: String, required: false, default: "" },
+    visible: { type: Boolean, required: true },
+    mandatory: { type: Boolean, required: false, default: false },
+
+    icon: { type: [String, Object], required: false, default: () => DEFAULT_ICON },
+    title: { type: String, required: false, default: "" },
+
+    message: { type: [String, Array], required: false, default: () => [] },
+
+    button: { type: [DialogButton, Array], required: false, default: () => [] },
+  },
+  emits: [
+    "confirm",
+  ],
+
+  data() {
+  },
+  computed: {
+    messages(): {  },
+    buttons() {  },
+  },
+
+  methods: {
+    confirm(button) {
+      this.$emit("confirm", button);
+    },
+
+    //
+    // event
+    onButtonClicked(button) {
+      this.confirm(button);
+    },
+    onInput(visible) {
+      if (!visible) {
+        this.confirm(undefined);
+      }
+    },
+  },
+};
+</script>
+
+
+<style>
+</style>
