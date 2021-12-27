88965be2b490b0f3291ba6e858fa6106a887546b
Simpler one (Koba) <40133262+simpler-one@users.noreply.github.com>
2021-12-28 01:38:57 +0900

    Create index.ts

---
+++ components/modal-dialog/src/components/ModalDialog/index.ts
@@ -0,0 +1,5 @@
+export { default } from "./ModalDialog";
+export { default as ModalDialogProps } from "./ModalDialogProps";
+export { default as ModalDialogHelper } from "./ModalDialogHelper";
+export { default as DialogButton } from "./DialogButton";
+export { default as DialogIcon } from "./DialogIcon";
