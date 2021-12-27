c8368539ebb53e6b8f3fc10e29c3ab545e6c53d0
Simpler one (Koba) <40133262+simpler-one@users.noreply.github.com>
2021-12-28 01:56:06 +0900

    Create default.vue

---
+++ components/modal-dialog/src/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <ModalDialog v-bind="$dialog.props" v-on="$dialog.listeners" />
+
+  <slot /> <!-- or <nuxt /> -->
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+
+<style>
+</style>
