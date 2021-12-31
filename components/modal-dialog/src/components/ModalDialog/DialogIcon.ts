f18a44d6a12af123f224764874d4d677e49bd0a5
Simpler one (Koba) <40133262+simpler-one@users.noreply.github.com>
2021-12-31 20:55:50 +0900

    Update DialogIcon.ts

--- components/modal-dialog/src/components/ModalDialog/DialogIcon.ts
+++ components/modal-dialog/src/components/ModalDialog/DialogIcon.ts
@@ -1,6 +1,8 @@
 export default class DialogIcon {
     public readonly name: string
     public readonly color: string
+
+    public static Empty: DialogIcon = Object.freeze(new DialogIcon("", ""));
     
     public constructor(name: string, color: string) {
         this.name = name;
