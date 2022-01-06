1c5de9342d0158868f6a24271dc0a71396189c1d
Simpler one (Koba) <40133262+simpler-one@users.noreply.github.com>
2022-01-07 08:49:39 +0900

    Update DialogButton.ts

--- components/modal-dialog/src/components/ModalDialog/DialogButton.ts
+++ components/modal-dialog/src/components/ModalDialog/DialogButton.ts
@@ -1,18 +1,18 @@
 export default class DialogButton {
-    public text: string
-    public icon: string
-    public class: string
-    public props: object
+    public text: string;
+    public icon: string;
+    public class: string;
+    public props: object;
 
-    public constructor(text: string, icon: string = "", class: string = "", props: object = undefined) {
+    public constructor(text: string, icon: string = "", cls: string = "", props: object = undefined) {
         this.text = text;
         this.icon = icon;
-        this.class = class;
+        this.class = cls;
         this.props = props ?? {};
     }
 
-    public static constant(text: string, icon: string = "", class: string = "", props: object = undefined): DialogButton {
-        return Object.freeze(new DialogButton());
+    public static constant(text: string, icon: string = "", cls: string = "", props: object = undefined): DialogButton {
+        return Object.freeze(new DialogButton(text, icon, cls, props));
     }
 
     public clone(mutable=true) {
